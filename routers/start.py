d96e49925e95a7c5ce1ff36e6ddf985ffd1877eb
Maksat <maksat@gmail.com>
2026-06-28 10:29:40 +0500

    Make start router strict to command only

--- routers/start.py
+++ routers/start.py
@@ -1,13 +1,21 @@
+import logging
 from aiogram import Router, types
-from aiogram.filters import CommandStart
-# Импортируем клавиатуру из файла меню
-from routers.menu import main_menu
+from aiogram.filters import Command
+from routers.menu import get_main_menu_keyboard
 
 router = Router()
 
-@router.message(CommandStart())
+@router.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer(
-        "Привет! Добро пожаловать в бот АА Наурыз. Используй меню для навигации.", 
-        reply_markup=main_menu
+    logging.info(f"Пользователь {message.from_user.id} нажал /start")
+    
+    welcome_text = (
+        "🕊 **Добро пожаловать в телеграм-бот группы АА «Наурыз»!**\n\n"
+        "Этот бот создан для поддержки участников нашего Содружества.\n"
+        "Здесь ты можешь узнать актуальное расписание живых встреч, найти спонсора "
+        "или предложить свою помощь в качестве наставника.\n\n"
+        "Пожалуйста, выбери интересующий раздел в меню ниже 👇"
     )
+    
+    kb = await get_main_menu_keyboard()
+    await message.answer(welcome_text, parse_mode="Markdown", reply_markup=kb)
