0f76421b2316400fdc14b96a5839a6ccb9d2446d
Maksat <maksat@gmail.com>
2026-08-16 00:32:35 +0500

    Fix SponsorForm states with all fields

--- routers/states.py
+++ routers/states.py
@@ -1,5 +1,10 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class SponsorForm(StatesGroup):
-    gender = State()   # Шаг 1: Пол
-    phone = State()    # Шаг 2: Телефон
+    name = State()         # Шаг 1: Имя
+    gender = State()       # Шаг 2: Пол
+    age = State()          # Шаг 3: Возраст
+    sobriety = State()     # Шаг 4: Трезвость
+    city = State()         # Шаг 5: Город
+    program_info = State() # Шаг 6: Опыт / Программа
+    phone = State()        # Шаг 7: Телефон
