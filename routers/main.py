1cfcdef0f2fd62725e77de1c91d5e42d78339bec
Maksat <maksat@gmail.com>
2026-08-16 10:54:30 +0500

    Fix scheduler: added max_instances and CronTrigger for stable job execution

--- routers/main.py
+++ routers/main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from pytz import utc
+from apscheduler.triggers.cron import CronTrigger
 
 from config import TOKEN 
 from routers.start import router as start_router
@@ -26,17 +26,21 @@ async def main():
     dp = Dispatcher()
     dp.include_routers(start_router, menu_router, form_router, sponsors_router, admin_router, help_router, schedules_router, reflections_router)
 
-    scheduler = AsyncIOScheduler(timezone=utc)
+    # Настраиваем планировщик с разрешением параллельных запусков (max_instances)
+    job_defaults = {
+        'max_instances': 3
+    }
+    scheduler = AsyncIOScheduler(timezone="UTC", job_defaults=job_defaults)
     
     # 06:00 Алматы = 01:00 UTC
-    scheduler.add_job(send_daily_reflection_to_channel, 'cron', hour=1, minute=0, args=[bot])
+    scheduler.add_job(send_daily_reflection_to_channel, CronTrigger(hour=1, minute=0), args=[bot])
     # 06:30 Алматы = 01:30 UTC
-    scheduler.add_job(send_morning_prayer_to_channel, 'cron', hour=1, minute=30, args=[bot])
+    scheduler.add_job(send_morning_prayer_to_channel, CronTrigger(hour=1, minute=30), args=[bot])
     # 23:00 Алматы = 18:00 UTC
-    scheduler.add_job(send_evening_prayer_to_channel, 'cron', hour=18, minute=0, args=[bot])
+    scheduler.add_job(send_evening_prayer_to_channel, CronTrigger(hour=18, minute=0), args=[bot])
     
     scheduler.start()
-    logging.info("Планировщик запущен: 06:00 (размышления), 06:30 (утр. молитва), 23:00 (веч. молитва) по Алматы.")
+    logging.info("Планировщик запущен с поддержкой параллельных задач (06:00, 06:30, 23:00 по Алматы).")
 
     await dp.start_polling(bot)
 
