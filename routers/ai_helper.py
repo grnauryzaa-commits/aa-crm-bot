b8ffa135c665d65e5e2da8c8c659da613a876cb6
Maksat <maksat@gmail.com>
2026-09-11 15:45:08 +0500

    Fix Groq import and requirements

--- routers/ai_helper.py
+++ routers/ai_helper.py
@@ -1,7 +1,6 @@
 import os
 from groq import Groq
 
-# Инициализация официального клиента Groq
 client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
 
 async def ask_ai_for_beginner(user_message: str) -> str:
